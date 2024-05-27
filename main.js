ae5293d98ef7241b59d5ce8660c0e12c676ed4e5
Kgopal123 <168902712+Kgopal123@users.noreply.github.com>
2024-05-27 15:40:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,207 @@
+let quizData = [
+    {
+      question: "Which of the following international organizations is primarily responsible for setting global labor standards, including those related to child labor?",
+      options: ["World Heath Organzation", "International Labour Organization", "United Nations Educational, Scientific and Cultural Organization ", "International Monetary Fund"],
+      correct: "International Labour Organization",
+    },
+    {
+      question: "What is the term for work that deprives children of their childhood, potential, and dignity, and is harmful to their physical and mental development?",
+      options: ["Child Apprenticeship", "Child Exploitation", "Child Labour", "Child Mentorship"],
+      correct: "Child Labour",
+    },
+    {
+      question:
+        "Which of the following is NOT a commonly cited reason for the prevalence of child labor?",
+      options: [
+        "Poverty",
+        "Lack of access to education",
+        "High unemployment rates among adults",
+        "Advanced technological infrastructure",
+      ],
+      correct: "Advanced technological infrastructure",
+    },
+    {
+      question: "Which of the following statements about child labor is true?",
+      options: [" Child labor is only a problem in developing countries.",
+       "Child labor does not affect children's education.",
+        "Child labor can lead to long-term physical and psychological harm.", 
+        " Child labor is always legal in all forms and circumstances."],
+      correct: "Child labor can lead to long-term physical and psychological harm.",
+    },
+    {
+      question: "In which sector is child labor most prevalent globally?",
+      options: [
+        "Agriculture",
+        "Manufacturing",
+        "Services",
+        "Mining",
+      ],
+      correct: "Agriculture",
+    },
+
+
+    {
+      question: "According to the International Labour Organization, what is the minimum age for employment that is generally recommended?",
+      options: [
+        "Age 12",
+        "Age 16",
+        "Age 15",
+        "Age 18",
+      ],
+      correct: "Age 15",
+    },
+  ];
+  
+  const quizContainer = document.querySelector(".quiz-container");
+  const question = document.querySelector(".quiz-container .question");
+  const options = document.querySelector(".quiz-container .options");
+  const nextBtn = document.querySelector(".quiz-container .next-btn");
+  const quizResult = document.querySelector(".quiz-result");
+  const startBtnContainer = document.querySelector(".start-btn-container");
+  const startBtn = document.querySelector(".start-btn-container .start-btn");
+  
+  let questionNumber = 0;
+  let score = 0;
+  const MAX_QUESTIONS = 5;
+  let timerInterval;
+  
+  const shuffleArray = (array) => {
+    return array.slice().sort(() => Math.random() - 0.5);
+  };
+  
+  quizData = shuffleArray(quizData);
+  
+  const resetLocalStorage = () => {
+    for (i = 0; i < MAX_QUESTIONS; i++) {
+      localStorage.removeItem(`userAnswer_${i}`);
+    }
+  };
+  
+  resetLocalStorage();
+  
+  const checkAnswer = (e) => {
+    let userAnswer = e.target.textContent;
+    if (userAnswer === quizData[questionNumber].correct) {
+      score++;
+      e.target.classList.add("correct");
+    } else {
+      e.target.classList.add("incorrect");
+    }
+  
+    localStorage.setItem(`userAnswer_${questionNumber}`, userAnswer);
+  
+    let allOptions = document.querySelectorAll(".quiz-container .option");
+    allOptions.forEach((o) => {
+      o.classList.add("disabled");
+    });
+  };
+  
+  const createQuestion = () => {
+    clearInterval(timerInterval);
+  
+    let secondsLeft = 9;
+    const timerDisplay = document.querySelector(".quiz-container .timer");
+    timerDisplay.classList.remove("danger");
+  
+    timerDisplay.textContent = `Time Left: 10 seconds`;
+  
+    timerInterval = setInterval(() => {
+      timerDisplay.textContent = `Time Left: ${secondsLeft
+        .toString()
+        .padStart(2, "0")} seconds`;
+      secondsLeft--;
+  
+      if (secondsLeft < 3) {
+        timerDisplay.classList.add("danger");
+      }
+  
+      if (secondsLeft < 0) {
+        clearInterval(timerInterval);
+        displayNextQuestion();
+      }
+    }, 1000);
+  
+    options.innerHTML = "";
+    question.innerHTML = `<span class='question-number'>${
+      questionNumber + 1
+    }/${MAX_QUESTIONS}</span>${quizData[questionNumber].question}`;
+  
+    const shuffledOptions = shuffleArray(quizData[questionNumber].options);
+  
+    shuffledOptions.forEach((o) => {
+      const option = document.createElement("button");
+      option.classList.add("option");
+      option.innerHTML = o;
+      option.addEventListener("click", (e) => {
+        checkAnswer(e);
+      });
+      options.appendChild(option);
+    });
+  };
+  
+  const retakeQuiz = () => {
+    questionNumber = 0;
+    score = 0;
+    quizData = shuffleArray(quizData);
+    resetLocalStorage();
+  
+    createQuestion();
+    quizResult.style.display = "none";
+    quizContainer.style.display = "block";
+  };
+  
+  const displayQuizResult = () => {
+    quizResult.style.display = "flex";
+    quizContainer.style.display = "none";
+    quizResult.innerHTML = "";
+  
+    const resultHeading = document.createElement("h2");
+    resultHeading.innerHTML = `You have scored ${score} out of ${MAX_QUESTIONS}.`;
+    quizResult.appendChild(resultHeading);
+  
+    for (let i = 0; i < MAX_QUESTIONS; i++) {
+      const resultItem = document.createElement("div");
+      resultItem.classList.add("question-container");
+  
+      const userAnswer = localStorage.getItem(`userAnswer_${i}`);
+      const correctAnswer = quizData[i].correct;
+  
+      let answeredCorrectly = userAnswer === correctAnswer;
+  
+      if (!answeredCorrectly) {
+        resultItem.classList.add("incorrect");
+      }
+  
+      resultItem.innerHTML = `<div class="question">Question ${i + 1}: ${
+        quizData[i].question
+      }</div>
+      <div class="user-answer">Your answer: ${userAnswer || "Not Answered"}</div>
+      <div class="correct-answer">Correct answer: ${correctAnswer}</div>`;
+  
+      quizResult.appendChild(resultItem);
+    }
+  
+    const retakeBtn = document.createElement("button");
+    retakeBtn.classList.add("retake-btn");
+    retakeBtn.innerHTML = "Retake Quiz";
+    retakeBtn.addEventListener("click", retakeQuiz);
+    quizResult.appendChild(retakeBtn);
+  };
+  
+  const displayNextQuestion = () => {
+    if (questionNumber >= MAX_QUESTIONS - 1) {
+      displayQuizResult();
+      return;
+    }
+  
+    questionNumber++;
+    createQuestion();
+  };
+  
+  nextBtn.addEventListener("click", displayNextQuestion);
+  
+  startBtn.addEventListener("click", () => {
+    startBtnContainer.style.display = "none";
+    quizContainer.style.display = "block";
+    createQuestion();
+  });
